bda5555d3b402e9a2c0d7084c0276513c8c442f2
EyEmilyKim surf <myue4555@gmail.com>
2022-08-22 23:39:22 +0900

    [UPDATE] 생활코딩 - Collection (6/9: Iterator, next() 용법) // fix : syntax error

--- TIL/java_tutorials/src/y22/m08/collection/SetDemo1.java
+++ TIL/java_tutorials/src/y22/m08/collection/SetDemo1.java
@@ -34,7 +34,7 @@ public class SetDemo1 {
 			System.out.println(hi.next());
 		}
 		if(hi.hasNext()) {
-			while(hi.hasNext()) {System.out.println(hi.next());
-		}else { System.out.println("false");}
+			while(hi.hasNext()) { System.out.println(hi.next()); }
+		}else { System.out.println("false"); }
 	}
 }
