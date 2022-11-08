d0ed203c162daf35e2d593209d6da8cddfa56fb8
EyEmilyKim surf <myue4555@gmail.com>
2022-11-08 16:47:36 +0900

    [뉴렉처 - Spring] XML Configuration을 Java Configuration으로 변경하기

--- TIL/spring/src/spring/di/Program.java
+++ TIL/spring/src/spring/di/Program.java
@@ -1,13 +1,7 @@
 package spring.di;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import spring.di.entity.Exam;
-import spring.di.entity.NewlecExam;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.di.ui.ExamConsole;
 
 public class Program {
@@ -22,7 +16,9 @@ public class Program {
 		console.setExam(exam);
 		*/
 		
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		ApplicationContext context = 
+//				new ClassPathXmlApplicationContext("spring/di/setting.xml");
+				new AnnotationConfigApplicationContext(NewlecDIConfig.class);
 		
 //		Exam exam = context.getBean(Exam.class);
 //		System.out.println(exam.toString());
--- TIL/spring/src/spring/di/entity/NewlecExam.java
+++ TIL/spring/src/spring/di/entity/NewlecExam.java
@@ -1,9 +1,7 @@
 package spring.di.entity;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
 
-@Service
 public class NewlecExam implements Exam {
 
 	@Value("20")
