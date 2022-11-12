66a4fd0613eb90ba70e59ab4708245b57f935a6a
EyEmilyKim surf <myue4555@gmail.com>
2022-11-12 23:14:02 +0900

    [뉴렉처 - Spring 02] 순수 자바로 AOP 구현해보기 (proxy)

---
+++ TIL/spring/src/spring/aop/Program.java
@@ -0,0 +1,47 @@
+package spring.aop;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import spring.aop.entity.Exam;
+import spring.aop.entity.NewlecExam;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Exam exam = new NewlecExam(1,1,1,1);
+		
+		Exam proxy = (Exam) Proxy.newProxyInstance(NewlecExam.class.getClassLoader(), 
+				new Class[] {Exam.class}, 
+				new InvocationHandler() {
+					
+					@Override
+					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+						//이곳에 Handler 의 core 작성
+						//위아래에 곁다리 업무, 가운데에 실제 업무 호출하는 코드
+						
+						long start = System.currentTimeMillis();
+						
+						//실제 업무 호출하는 로직
+						Object result = method.invoke(exam, args);
+						
+						long end = System.currentTimeMillis();
+						
+						String message = (end - start)+"ms 시간이 걸렸습니다";
+						System.out.println(message);
+						
+						return result;
+					}
+				} 
+				); 
+		
+//		System.out.printf("total is %d\n", exam.total());
+		
+		System.out.printf("total is %d\n", proxy.total());
+		System.out.printf("avg is %f\n", proxy.avg());
+		
+	}
+
+}
