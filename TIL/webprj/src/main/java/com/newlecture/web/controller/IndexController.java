4e17eed0f23bc81d77428190e658cc222fe833a7
EyEmilyKim kosea4 <myue4555@gmail.com>
2022-12-09 16:26:09 +0900

    [뉴렉처 - Spring 03] 스프링 컨트롤러 IndexController 작성하기

---
+++ TIL/webprj/src/main/java/com/newlecture/web/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.newlecture.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class IndexController implements Controller {
+
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("IndexController called");
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("data", "Hello Sping MVC~");
+		mav.setViewName("index.jsp");
+		return mav;
+	}
+}
