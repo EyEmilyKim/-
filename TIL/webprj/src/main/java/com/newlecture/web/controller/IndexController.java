a772f780e539cd40ef84e8d4aa8b1698cc94375e
EyEmilyKim surf <myue4555@gmail.com>
2023-04-17 22:30:52 +0900

    [뉴렉처 - Spring 03] 27 - Annotation으로 URL 매핑하기

--- TIL/webprj/src/main/java/com/newlecture/web/controller/IndexController.java
+++ TIL/webprj/src/main/java/com/newlecture/web/controller/IndexController.java
@@ -1,18 +1,22 @@
 package com.newlecture.web.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+@Controller
+public class IndexController {
 
-public class IndexController implements Controller {
-
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		System.out.println("IndexController called");
-		ModelAndView mav = new ModelAndView("root.index");
-		mav.addObject("data", "Hello Sping MVC~");
-//		mav.setViewName("/WEB-INF/view/index.jsp");
-		return mav;
+	@RequestMapping("/index")
+	public void aaaa() {
+		System.out.println("aaaa() called");
 	}
+	
+//	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//		System.out.println("IndexController called");
+//		ModelAndView mav = new ModelAndView("root.index");
+//		mav.addObject("data", "Hello Sping MVC~");
+////		mav.setViewName("/WEB-INF/view/index.jsp");
+//		return mav;
+//	}
+
 }
