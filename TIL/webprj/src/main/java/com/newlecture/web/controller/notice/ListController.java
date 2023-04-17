20cd92c3b365909d030aea7b6e0f523e19046328
EyEmilyKim surf <myue4555@gmail.com>
2023-04-17 21:35:31 +0900

    [뉴렉처 - Spring 03] 25 - 객체 DI를 Annotaion으로 변경하기

--- TIL/webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
+++ TIL/webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -13,11 +14,14 @@ import com.newlecture.web.service.NoticeService;
 
 public class ListController implements Controller {
 
+	@Autowired
 	private NoticeService noticeService;
 	
-	public void setNoticeService(NoticeService noticeService) {
-		this.noticeService = noticeService;
-	}
+//	@Autowired
+//	public void setNoticeService(NoticeService noticeService) {
+//		this.noticeService = noticeService;
+//		System.out.println("Hello");
+//	}
 
 
 
