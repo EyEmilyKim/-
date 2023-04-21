f30fdff169c89ece20cd46fba5a8366013f5f9f4
EyEmilyKim surf <myue4555@gmail.com>
2023-04-21 12:46:50 +0900

    [뉴렉처 - Spring 03] 36 - QueryString 입력 #2 : 변수명 별칭과 기본 값처리

--- TIL/webprj/src/main/java/com/newlecture/web/controller/customer/NoticeController.java
+++ TIL/webprj/src/main/java/com/newlecture/web/controller/customer/NoticeController.java
@@ -2,11 +2,10 @@ package com.newlecture.web.controller.customer;
 
 import java.sql.SQLException;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.newlecture.web.service.NoticeService;
 
@@ -18,10 +17,10 @@ public class NoticeController {
 	private NoticeService noticeService;
 	
 	@RequestMapping("list")
-	public String list(String p) throws ClassNotFoundException, SQLException {
+	public String list(@RequestParam(name="p", defaultValue="1") int page) throws ClassNotFoundException, SQLException {
 		System.out.println("NoticeController > list() called");
 //		String p = request.getParameter("p");
-		System.out.println("page : "+p);
+		System.out.println("page : "+page);
 		
 //		List<Notice> list = noticeService.getList(1, "title", "");
 		
