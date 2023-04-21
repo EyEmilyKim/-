117e016fcac7998db7e760a2cf539b726341c3e4
EyEmilyKim surf <myue4555@gmail.com>
2023-04-21 19:10:51 +0900

    [뉴렉처 - Spring 03] 44 - POST 입력 #6(물리경로 얻기와 파일 저장하기)

--- TIL/webprj/src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
+++ TIL/webprj/src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
@@ -1,5 +1,12 @@
 package com.newlecture.web.controller.admin.board;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,7 +15,10 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller("adminNoticeController")
 @RequestMapping("/admin/board/notice/")
 public class NoticeController {
-
+	
+	@Autowired
+	private ServletContext ctx;
+	
 //	@RequestMapping("list")
 	public String list() {
 		
@@ -17,11 +27,26 @@ public class NoticeController {
 	
 	@RequestMapping("reg")
 	@ResponseBody
-	public String reg(String title, String content, MultipartFile file, String category, String[] foods, String food) {
+	public String reg(String title, String content, MultipartFile file, String category, String[] foods, String food, HttpServletRequest request) throws IllegalStateException, IOException {
 		
 		long size = file.getSize();
 		String fileName = file.getOriginalFilename();
 		System.out.printf("filename : %s, filesize : %d\n", fileName, size);
+		
+		String webPath = "/static/upload";
+//		ServletContext ctx = request.getServletContext();
+		String realPath = ctx.getRealPath(webPath);
+		System.out.printf("realPath : %s\n", realPath);
+		//업로드하기 위한 경로가 없을 경우 dir 만들어줘~
+		File savePath = new File(realPath);
+		if(!savePath.exists())
+			savePath.mkdirs();
+		
+		realPath += File.separator + fileName;
+		File saveFile = new File(realPath);
+		
+		file.transferTo(saveFile);
+		
 		
 		for(String f : foods)
 			System.out.println(f);
