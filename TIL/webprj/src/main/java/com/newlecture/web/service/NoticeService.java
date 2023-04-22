46667b84ed9603a1887f2ad487036a8fbdb858a0
EyEmilyKim surf <myue4555@gmail.com>
2023-04-23 00:05:39 +0900

    [뉴렉처 - Spring 04] 완강 후 개인 메모, 자잘한 정리..

--- TIL/webprj/src/main/java/com/newlecture/web/service/NoticeService.java
+++ TIL/webprj/src/main/java/com/newlecture/web/service/NoticeService.java
@@ -7,10 +7,10 @@ import com.newlecture.web.entity.Notice;
 
 public interface NoticeService {
 
-	List<Notice> getList(int page, String field, String query) throws ClassNotFoundException, SQLException{}
-	int getCount() throws ClassNotFoundException, SQLException {}
-	int insert(Notice notice) throws SQLException, ClassNotFoundException {}
-	int update(Notice notice) throws SQLException, ClassNotFoundException {}
-	int delete(int id) throws ClassNotFoundException, SQLException {}
+	List<Notice> getList(int page, String field, String query) throws ClassNotFoundException, SQLException;
+	int getCount() throws ClassNotFoundException, SQLException;
+	int insert(Notice notice) throws SQLException, ClassNotFoundException;
+	int update(Notice notice) throws SQLException, ClassNotFoundException;
+	int delete(int id) throws ClassNotFoundException, SQLException;
 
 }
