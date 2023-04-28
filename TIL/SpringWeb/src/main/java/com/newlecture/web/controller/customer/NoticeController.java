dfa78bba120f14c9a0da88bfcc16b4c26f4b3e68
EyEmilyKim surf <myue4555@gmail.com>
2023-04-28 18:24:55 +0900

    [뉴렉처 - Spring 05] 07 - 사용자 공지사항을 위한 MVC 구현하기

---
+++ TIL/SpringWeb/src/main/java/com/newlecture/web/controller/customer/NoticeController.java
@@ -0,0 +1,20 @@
+package com.newlecture.web.controller.customer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class NoticeController {
+
+		@RequestMapping("/customer/notice/list")
+		public String list(Model model) {
+			model.addAttribute("test", "Hello~");
+			return "/customer/notice/list.jsp";
+		}
+		
+		@RequestMapping("/customer/notice/detail")
+		public String detail() {
+			return "/customer/notice/detail.jsp";
+		}
+}
