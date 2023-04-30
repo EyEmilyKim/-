dda38d9a638dc315f2aa66dff4e742f65160690c
EyEmilyKim surf <myue4555@gmail.com>
2023-04-30 21:57:57 +0900

    [뉴렉처 - Spring 05] 21 - Mapper 객체 만들기

--- TIL/SpringWeb/src/main/java/com/newlecture/web/dao/NoticeDao.java
+++ TIL/SpringWeb/src/main/java/com/newlecture/web/dao/NoticeDao.java
@@ -2,10 +2,12 @@ package com.newlecture.web.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Select;
+
 import com.newlecture.web.entity.Notice;
 
 public interface NoticeDao {
-
+	@Select("Select * from notice")
 	List<Notice> getList();
 
 	Notice get(int id);
