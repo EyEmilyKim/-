e8c5824509fef28ac7da6736a1cb0d81a7a141d0
EyEmilyKim surf <myue4555@gmail.com>
2023-05-08 22:51:33 +0900

    [뉴렉처 - Spring 05] 23 - Mapper 객체를 이용해서 공지목록 출력하기

--- TIL/SpringWeb/src/main/java/com/newlecture/web/controller/customer/NoticeController.java
+++ TIL/SpringWeb/src/main/java/com/newlecture/web/controller/customer/NoticeController.java
@@ -1,19 +1,31 @@
 package com.newlecture.web.controller.customer;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.newlecture.web.entity.Notice;
+import com.newlecture.web.service.NoticeService;
 
 @Controller
 @RequestMapping("/customer/notice/")
 public class NoticeController {
 
-		@RequestMapping("list")
-		public String list(Model model) {
-			model.addAttribute("test", "Hello DevTools~");
-			return "customer.notice.list"; // for TilesViewResolver
+	@Autowired
+	private NoticeService service;
+	
+	@RequestMapping("list")
+	public String list(Model model) {
+		
+		List<Notice> list = service.getList();
+		
+		model.addAttribute("list", list);
+		return "customer.notice.list"; // for TilesViewResolver
 //			return "customer/notice/list"; // for ResourceViewResolver
-		}
+	}
 		
 		@RequestMapping("detail")
 		public String detail() {
--- TIL/SpringWeb/src/main/java/com/newlecture/web/dao/NoticeDao.java
+++ TIL/SpringWeb/src/main/java/com/newlecture/web/dao/NoticeDao.java
@@ -2,10 +2,12 @@ package com.newlecture.web.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import com.newlecture.web.entity.Notice;
 
+@Mapper
 public interface NoticeDao {
 	@Select("Select * from notice")
 	List<Notice> getList();
