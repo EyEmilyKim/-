6909948ffd895a3ae57b5ad3a02d16d96752add1
EyEmilyKim surf <myue4555@gmail.com>
2023-05-09 17:56:02 +0900

    [뉴렉처 - Spring 05] 25 - MyBatis Column 매핑

--- TIL/SpringWeb/src/main/java/com/newlecture/web/dao/NoticeDao.java
+++ TIL/SpringWeb/src/main/java/com/newlecture/web/dao/NoticeDao.java
@@ -3,6 +3,8 @@ package com.newlecture.web.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import com.newlecture.web.entity.Notice;
@@ -10,6 +12,11 @@ import com.newlecture.web.entity.NoticeView;
 
 @Mapper
 public interface NoticeDao {
+	
+//	@Results({
+//		@Result(property = "regdate", column = "reg_date"),
+//		@Result(property = "memberName", column = "member_name")
+//	})
 	@Select("Select * from noticeview")
 	List<NoticeView> getList();
 
