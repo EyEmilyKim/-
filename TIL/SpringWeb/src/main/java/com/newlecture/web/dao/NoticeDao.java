316b2dae135d3cc1bd2726ab1056e8380e35bfbf
EyEmilyKim surf <myue4555@gmail.com>
2023-05-10 18:53:19 +0900

    [뉴렉처 - Spring 05] 31 - Mapper 구현하기 #1 (getCount/getView)

--- TIL/SpringWeb/src/main/java/com/newlecture/web/dao/NoticeDao.java
+++ TIL/SpringWeb/src/main/java/com/newlecture/web/dao/NoticeDao.java
@@ -15,14 +15,15 @@ public interface NoticeDao {
 
 	List<NoticeView> getViewList(int offset, int size, String field, String query);
 	int getCount(String field, String query);
-
 	NoticeView getView(int id);
+
 	Notice getNext(int id);
 	Notice getPrev(int id);
 	
-	int updatePubAll(int[] pubIds, int[] closeIds);
-	int deleteAll(int[] ids);
 	int update(Notice notice);
 	int delete(int id);
 	int insert(Notice notice);
+	
+	int deleteAll(int[] ids);
+	int updatePubAll(int[] pubIds, int[] closeIds);
 }
