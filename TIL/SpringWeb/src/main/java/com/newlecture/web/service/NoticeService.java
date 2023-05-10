ed6704c1be988a88c80c223c84a2aa36f68bff67
EyEmilyKim surf <myue4555@gmail.com>
2023-05-10 16:54:10 +0900

    [뉴렉처 - Spring 05] 29 - NoticeService 인터페이스 정의하기

--- TIL/SpringWeb/src/main/java/com/newlecture/web/service/NoticeService.java
+++ TIL/SpringWeb/src/main/java/com/newlecture/web/service/NoticeService.java
@@ -7,8 +7,38 @@ import com.newlecture.web.entity.NoticeView;
 
 public interface NoticeService {
 
-	List<NoticeView> getList(int page, String field, String query);
+	// - 목록 페이지를 요청할 때
+	List<NoticeView> getViewList();
 
-	Notice get(int id);
-	
+	int getCount();
+
+	// - 검색을 요청할 때
+	List<NoticeView> getViewList(String field, String query);
+
+	// - 페이지 버튼 눌러 요청할 때
+	List<NoticeView> getViewList(int page, String field, String query);
+
+	int getCount(String field, String query);
+
+	// - 자세한 페이지 요청할 때
+	NoticeView get(int id);
+
+	Notice getNext(int id);
+
+	Notice getPrev(int id);
+
+	// - 일괄공개를 요청할 때
+	int updatePubAll(int[] pubIds, int[] closeIds);
+
+	// - 일괄삭제를 요청할 때
+	int deleteAll(int[] ids);
+
+	// - 수정 페이지 요청할 때
+	int update(Notice notice);
+
+	int delete(int id);
+
+	// - 목록에서 글쓰기 버튼
+	int insert(Notice notice);
+
 }
