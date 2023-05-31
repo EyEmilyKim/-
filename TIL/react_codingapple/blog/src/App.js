e3e1bc237cff943fa098faacc537acb4c7f5cba6
EyEmilyKim surf <myue4555@gmail.com>
2023-05-31 18:32:26 +0900

    [코딩애플 - react 2022] 3강 : state 쓰면 뭐가 좋냐면

--- TIL/react_codingapple/blog/src/App.js
+++ TIL/react_codingapple/blog/src/App.js
@@ -1,16 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-  let post = '강남 우동 맛집';
+  let post = '구름 닮은 빵';
+  let [글제목, b] = useState(['당근 거래 후기', '동현 동원 동물원']);
+  let [발행일, c] = useState(['2023-05-31', '2023-05-30']);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{color:'red', fontSize:'16px'}}>블로그 - 코딩애플과 함께 하는 React 연습</h4>
+        <h4>블로그 - 코딩애플과 함께 하는 React 연습</h4>
       </div>
-      <h4 id={post}>{ post }</h4>
+      <div className="list">
+        <h4>{ post }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[0] }</h4>
+        <p>{ 발행일[0] } 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>{ 발행일[1] } 발행</p>
+      </div>
     </div>
   );
 }
