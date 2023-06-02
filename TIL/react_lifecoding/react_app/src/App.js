ed4895393d78590a9be1ef88ff0caf7eb520275c
EyEmilyKim surf <myue4555@gmail.com>
2023-06-02 23:00:17 +0900

    [생활코딩 - React 2022] Self - components 폴더 따로 빼기, 코드 정리

--- TIL/react_lifecoding/react_app/src/App.js
+++ TIL/react_lifecoding/react_app/src/App.js
@@ -1,44 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+// import Header from './components/Header';
+import Nav from './components/Nav';
+import Article from './components/Article';
+import * as Components from './components';
 
-function Header(props){
-  // console.log('props', props.title);
-  return <header>
-    <h1><a href='/' onClick={(event)=>{
-      event.preventDefault();
-      props.onChangeMode();
-    }}>{props.title}</a></h1>
-  </header>
-}
-function Nav(props){
-  const lis = []
-  for(let i=0; i<props.topics.length; i++){
-    let t = props.topics[i];
-    lis.push(<li key={t.id}>
-      <a id={t.id} href={'/read/'+t.id} onClick={event  =>{
-      event.preventDefault();
-      props.onChangeMode(Number(event.target.id));
-    }}>{t.title}</a>
-    </li>)
-  }
-  return <nav>
-    <ol>
-      {lis}
-    </ol>
-  </nav>
-}
-function Article(props){
-  return <article>
-    <h2>{props.title}</h2>
-    {props.body}
-  </article>
-}
 
 function App() {
-  // const _mode = useState('WELCOME');
-  // const mode = _mode[0];
-  // const setMode = _mode[1];
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
   console.log('mode', mode);
@@ -58,21 +27,26 @@ function App() {
       if(topics[i].id === id){
         title = topics[i].title;
         body = topics[i].body;
+        break;
       } 
     }
     content = <Article title={title} body={body}></Article>
   }
+
   return (
     <div>
-      <Header title="WEB" onChangeMode={()=>{
+      <Components.Header title="WEB" onChangeMode={()=>{
         setMode('WELCOME');
         setId(null);
-      }}></Header>
+      }}></Components.Header>
+
       <Nav topics={topics} onChangeMode={(_id)=>{
         setMode('READ');
         setId(_id);
       }}></Nav>
-      {content}
+
+      <div className='content'>{content}</div>
+
     </div>
   );
 }
