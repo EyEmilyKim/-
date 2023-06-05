56f61562f1c96dacc1676be726b6b3196cba7dd1
EyEmilyKim surf <myue4555@gmail.com>
2023-06-05 23:39:44 +0900

    [코딩앙마 - React JS] #10 라우터 구현 react-router-dom

--- TIL/react_codingangma/voca/src/App.js
+++ TIL/react_codingangma/voca/src/App.js
@@ -1,14 +1,21 @@
 import Day from "./components/Day";
 import DayList from "./components/DayList";
+import EmptyPage from "./components/EmptyPage";
 import Header from "./components/Header";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Header />
-      <DayList />
-      <Day />
+      <Routes>
+        <Route path="/" element={<DayList />} />
+        <Route path="/day/:day" element={<Day />} />
+        <Route path="*" element={<EmptyPage />} />
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
