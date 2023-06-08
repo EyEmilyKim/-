495706cde706cc649f2da0b632133a9e0b06da65
EyEmilyKim surf <myue4555@gmail.com>
2023-06-08 21:56:47 +0900

    [코딩앙마 - React JS] 삭제 - 트랜지션 애니메이션

--- TIL/react_codingangma/voca/src/components/Word.js
+++ TIL/react_codingangma/voca/src/components/Word.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import './Word.css';
 
 export default function Word({ word: w }) {
   const [word, setWord] = useState(w);
   const [isShow, setIsShow] = useState(false);
   const [isDone, setIsDone] = useState(word.isDone);
+  const [isDeleting, setIsDeleting] = useState(false);
 
   function toggleShow() {
     setIsShow(!isShow);
@@ -29,22 +31,29 @@ export default function Word({ word: w }) {
 
   function del(){
     if(window.confirm('삭제하시겠습니까?')){
-      fetch(`http://localhost:3001/words/${word.id}`, {
-        method : 'DELETE',
-      }).then(res => {
-        if(res.ok){
-          setWord({id:0})
-        }
-      });
+      setIsDeleting(true);
+      setTimeout(()=>{
+
+        fetch(`http://localhost:3001/words/${word.id}`, {
+          method : 'DELETE',
+        }).then(res => {
+          if(res.ok){
+            setWord({id:0});
+          }
+        });
+        
+      }, 600);
     }
   }
 
   if(word.id === 0){
     return null;
   }
-  
+
+  const wordClassName = isDeleting ? "word-fade-out" : "";
+
   return (
-    <tr className={isDone ? "off" : ""}>
+    <tr className={`${isDone ? "off" : ""} ${wordClassName}`}>
       <td>
         <input type="checkbox" checked={isDone} onChange={toggleDone}/>
       </td>
