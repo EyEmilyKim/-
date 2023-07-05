c74bbffa1f640ef2916ac4fa1d7e2283638b0404
EyEmilyKim surf <myue4555@gmail.com>
2023-07-05 19:05:22 +0900

    [생활코딩 - React MUI] Container

--- TIL/react_lifecoding/react_mui/src/App.js
+++ TIL/react_lifecoding/react_mui/src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Article from './components/Article';
+import Container from '@mui/material/Container';
 
 function App() {
   return (
-    <div>
+    <Container fixed>
       <Header />
       <Nav />
       <Article />
-    </div>
+    </Container>
   );
 }
 
--- TIL/react_lifecoding/react_mui/src/components/Article.js
+++ TIL/react_lifecoding/react_mui/src/components/Article.js
@@ -5,7 +5,9 @@ export default function Article() {
   return (
     <article>
       <h2>Welcome</h2>
-      Hello web!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum accusamus perferendis dicta cumque sit vel voluptatem ex inventore doloremque reprehenderit. Modi minima autem explicabo odio ducimus. Earum nemo non alias.
+      Nostrum expedita qui odit corrupti beatae autem minus quia, quo repudiandae dolor quae ullam. Dolorem ratione repudiandae incidunt dolorum similique earum asperiores, ad eveniet voluptas laborum amet quis obcaecati exercitationem.
+      Nobis accusamus necessitatibus sunt cupiditate voluptate voluptas dolorum vitae itaque soluta? Provident dolore voluptatibus aliquid dignissimos quis, ex error ab nostrum at enim, minima et ipsum tenetur labore. Dolor, odio.
       <br />
       <ButtonGroup className='margin_right'>
         <Button variant='outlined'>Create</Button>
