19f09f15e182e22290d4971bfe24f220be4e8d32
EyEmilyKim surf <myue4555@gmail.com>
2023-07-05 19:20:48 +0900

    [생활코딩 - React MUI] Grid

--- TIL/react_lifecoding/react_mui/src/App.js
+++ TIL/react_lifecoding/react_mui/src/App.js
@@ -1,16 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import Article from './components/Article';
-import Container from '@mui/material/Container';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Article from "./components/Article";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 
 function App() {
   return (
     <Container fixed>
       <Header />
-      <Nav />
-      <Article />
+      <Grid container>
+        <Grid item xs="3">
+          <Nav />
+        </Grid>
+        <Grid item xs="8">
+          <Article />
+        </Grid>
+      </Grid>
     </Container>
   );
 }
