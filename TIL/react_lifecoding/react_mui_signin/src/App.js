e0f70c605b3569e3c4cbd3baf3c5aefe09cd65f0
EyEmilyKim surf <myue4555@gmail.com>
2023-07-11 18:54:11 +0900

    [생활코딩 - React MUI Signin] 로그인 Template 완성~

--- TIL/react_lifecoding/react_mui_signin/src/App.js
+++ TIL/react_lifecoding/react_mui_signin/src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TextField from "@mui/material/TextField";
+import {
+  Avatar,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  Link,
+  Typography,
+  Box,
+  Container
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, backgroundColor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="5">
+          Sign in
+        </Typography>
+
+        <TextField
+          label="Email Address"
+          name="email"
+          required
+          margin="dense"
+          fullWidth
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          label="Password"
+          type="password"
+          name="password"
+          required
+          margin="dense"
+          fullWidth
+          autoComplete="current-password"
+        />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          SIGN IN
+        </Button>
+
+        <Grid container justifyContent="flex-start">
+          <Grid item xs>
+            <Link>Forgot password?</Link>
+          </Grid>
+          <Grid item>
+            <Link>Sign Up</Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
   );
 }
 
