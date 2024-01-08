78d4e752e91f692f3100560ee92dc2bbcdc4cd9f
EyEmilyKim surf <myue4555@gmail.com>
2024-01-08 22:19:59 +0900

    [AboutTech - Session] session설정, session 확인 미들웨어

--- TIL/Session_AboutTech/index.js
+++ TIL/Session_AboutTech/index.js
@@ -17,7 +17,43 @@ app.use(
   })
 );
 
+// session 설정
+app.use(
+  session({
+    name: "session ID",
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+      httpOnly: false,
+      secure: false,
+    },
+  })
+);
 
+// session 확인 미들웨어
+app.use("/", (req, res, next) => {
+  try {
+    if (req.session.views) {
+      req.session.views++;
+    } else {
+      req.session.views=1;
+    }
+    console.log("session info", req.session);
+    next();
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+app.get("/session", (res, req) => {
+  console.log("/session requested");
+  // session을 받아오는 로직
+
+  res.statusCode(200).json("session information");
+});
 
 app.listen(8122, () => {
   console.log("server is on 8122...");
