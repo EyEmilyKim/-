46a7610fe06727806c57d25e4adf629b2176d2d1
EyEmilyKim surf <myue4555@gmail.com>
2024-01-08 23:08:18 +0900

    [AboutTech - Session] 로그인, session 생성..

--- TIL/Session_AboutTech/index.js
+++ TIL/Session_AboutTech/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const session = require("express-session");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const userDB = require("./Database");
 
 const app = express();
 dotenv.config();
@@ -38,7 +39,7 @@ app.use("/", (req, res, next) => {
     if (req.session.views) {
       req.session.views++;
     } else {
-      req.session.views=1;
+      req.session.views = 1;
     }
     console.log("session info", req.session);
     next();
@@ -52,7 +53,23 @@ app.get("/session", (res, req) => {
   console.log("/session requested");
   // session을 받아오는 로직
 
-  res.statusCode(200).json("session information");
+  res.status(200).json("session information");
+});
+
+app.post("/login", (req, res) => {
+  const userInfo = userDB.filter((item) => {
+    return item.email === req.body.email;
+  });
+
+  // session 생성
+  req.session.save(() => {
+    req.session.user = {
+      email: userInfo[0].email,
+      username: userInfo[0].username,
+    };
+    const data = req.session;
+    res.status(200).json({ data });
+  });
 });
 
 app.listen(8122, () => {
