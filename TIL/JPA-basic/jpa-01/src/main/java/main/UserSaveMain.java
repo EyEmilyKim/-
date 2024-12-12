76e1737245a22a0009647b5fea2f0b2e7ca536b8
EyEmilyKim surf <myue4555@gmail.com>
2024-12-12 16:07:42 +0900

    [JPA 기초] 01. 일단 해보기

---
+++ TIL/JPA-basic/jpa-01/src/main/java/main/UserSaveMain.java
@@ -0,0 +1,34 @@
+package main;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import jpabasic.reserve.domain.User;
+
+public class UserSaveMain {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabegin");
+		
+		EntityManager entityManager = emf.createEntityManager();
+		EntityTransaction transaction = entityManager.getTransaction();
+		try {
+			transaction.begin();
+			
+			User user = new User("user@user.com", "user", LocalDateTime.now());
+			entityManager.persist(user);
+			
+			transaction.commit();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			transaction.rollback();
+		} finally {
+			entityManager.close();
+		}
+		emf.close();
+	}
+
+}
